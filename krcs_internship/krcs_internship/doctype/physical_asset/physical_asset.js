cee4b0b44623a7274fa4d36afe1ef24660adf311
Ashlyne2001 <ashlyneokumu@gmail.com>
2025-07-25 15:13:37 +0300

    Updated Intern Activities: renamed fields and added description

--- krcs_internship/krcs_internship/doctype/physical_asset/physical_asset.js
+++ krcs_internship/krcs_internship/doctype/physical_asset/physical_asset.js
@@ -1,7 +1,11 @@
 // Copyright (c) 2025, Ash_Ok and contributors
 // For license information, please see license.txt
 
+<<<<<<<< HEAD:krcs_internship/krcs_internship/doctype/physical_asset/physical_asset.js
 // frappe.ui.form.on("Physical Asset", {
+========
+// frappe.ui.form.on("Virtual Systems", {
+>>>>>>>> 6607e09b (Updated Intern Activities: renamed fields and added description):apps/krcs_internship/krcs_internship/krcs_internship/doctype/virtual_systems/virtual_systems.js
 // 	refresh(frm) {
 
 // 	},
--- krcs_internship/krcs_internship/doctype/physical_asset/physical_asset.py
+++ krcs_internship/krcs_internship/doctype/physical_asset/physical_asset.py
@@ -5,5 +5,9 @@
 from frappe.model.document import Document
 
 
+<<<<<<<< HEAD:krcs_internship/krcs_internship/doctype/physical_asset/physical_asset.py
 class PhysicalAsset(Document):
+========
+class VirtualSystems(Document):
+>>>>>>>> 6607e09b (Updated Intern Activities: renamed fields and added description):apps/krcs_internship/krcs_internship/krcs_internship/doctype/virtual_systems/virtual_systems.py
 	pass
